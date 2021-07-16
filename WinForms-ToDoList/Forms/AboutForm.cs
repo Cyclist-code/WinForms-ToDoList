0b981e088b0e546be896adb5cf72b49cc6714fd2
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-07-16 09:49:52 +0700

    Adding a window for application information and communication with the developer

---
+++ WinForms-ToDoList/Forms/AboutForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace WinForms_ToDoList.Forms
+{
+    public partial class AboutForm : MaterialForm
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green500, Primary.Green700, Primary.Green100, Accent.Green700, TextShade.WHITE);
+        }
+    }
+}
