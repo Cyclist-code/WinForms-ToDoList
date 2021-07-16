a733884396886187141a80380e198992f2d58afe
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-07-16 09:37:28 +0700

    Initial customization of the main window design

--- WinForms-ToDoList/Forms/MainForm.Designer.cs
+++ WinForms-ToDoList/Forms/MainForm.Designer.cs
@@ -29,10 +29,21 @@ namespace WinForms_ToDoList
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(750, 550);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(750, 550);
+            this.MinimumSize = new System.Drawing.Size(750, 550);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ToDoList App";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- WinForms-ToDoList/Forms/MainForm.cs
+++ WinForms-ToDoList/Forms/MainForm.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace WinForms_ToDoList
 {
-    public partial class MainForm : Form
+    public partial class MainForm : MaterialForm
     {
         public MainForm()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green500, Primary.Green700, Primary.Green100, Accent.Green700, TextShade.WHITE);
         }
     }
 }
