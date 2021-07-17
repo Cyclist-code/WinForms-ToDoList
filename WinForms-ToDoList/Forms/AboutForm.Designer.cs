244b288e03e3b086d1c304b66ba2118fdeb420c1
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-07-17 10:24:56 +0700

    Adding an icon for the application information window

--- WinForms-ToDoList/Forms/AboutForm.Designer.cs
+++ WinForms-ToDoList/Forms/AboutForm.Designer.cs
@@ -29,6 +29,7 @@ namespace WinForms_ToDoList.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -194,6 +195,7 @@ namespace WinForms_ToDoList.Forms
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
