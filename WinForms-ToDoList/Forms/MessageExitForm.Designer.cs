8cb0c75de952e009972a20d46d3540878392dda4
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-07-18 09:24:48 +0700

    Changing the interface of the exit from the application

--- WinForms-ToDoList/Forms/MessageExitForm.Designer.cs
+++ WinForms-ToDoList/Forms/MessageExitForm.Designer.cs
@@ -38,21 +38,21 @@ namespace WinForms_ToDoList.Forms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(6, 93);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(388, 20);
+            this.label1.Size = new System.Drawing.Size(351, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Вы действительно хотите выйти из приложения?";
             // 
             // ExitAppButton
             // 
             this.ExitAppButton.Depth = 0;
-            this.ExitAppButton.Location = new System.Drawing.Point(202, 138);
+            this.ExitAppButton.Location = new System.Drawing.Point(212, 120);
             this.ExitAppButton.MouseState = MaterialSkin.MouseState.HOVER;
             this.ExitAppButton.Name = "ExitAppButton";
             this.ExitAppButton.Primary = true;
-            this.ExitAppButton.Size = new System.Drawing.Size(90, 30);
+            this.ExitAppButton.Size = new System.Drawing.Size(70, 28);
             this.ExitAppButton.TabIndex = 1;
             this.ExitAppButton.Text = "Да";
             this.ExitAppButton.UseVisualStyleBackColor = true;
@@ -61,11 +61,11 @@ namespace WinForms_ToDoList.Forms
             // CancelExitAppButton
             // 
             this.CancelExitAppButton.Depth = 0;
-            this.CancelExitAppButton.Location = new System.Drawing.Point(298, 138);
+            this.CancelExitAppButton.Location = new System.Drawing.Point(288, 120);
             this.CancelExitAppButton.MouseState = MaterialSkin.MouseState.HOVER;
             this.CancelExitAppButton.Name = "CancelExitAppButton";
             this.CancelExitAppButton.Primary = true;
-            this.CancelExitAppButton.Size = new System.Drawing.Size(90, 30);
+            this.CancelExitAppButton.Size = new System.Drawing.Size(70, 28);
             this.CancelExitAppButton.TabIndex = 2;
             this.CancelExitAppButton.Text = "Нет";
             this.CancelExitAppButton.UseVisualStyleBackColor = true;
@@ -74,7 +74,7 @@ namespace WinForms_ToDoList.Forms
             // MessageExitForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(400, 180);
+            this.ClientSize = new System.Drawing.Size(370, 160);
             this.Controls.Add(this.CancelExitAppButton);
             this.Controls.Add(this.ExitAppButton);
             this.Controls.Add(this.label1);
