188358ce1757511d6bc6551b513ffd9ce6cccb21
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-07-18 14:34:52 +0700

    Adding a window for displaying messages

---
+++ WinForms-ToDoList/Forms/MessageForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace WinForms_ToDoList.Forms
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessageForm));
+            this.CloseButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.MessageLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Depth = 0;
+            this.CloseButton.Location = new System.Drawing.Point(288, 120);
+            this.CloseButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Primary = true;
+            this.CloseButton.Size = new System.Drawing.Size(70, 28);
+            this.CloseButton.TabIndex = 2;
+            this.CloseButton.Text = "ОК";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Depth = 0;
+            this.MessageLabel.Font = new System.Drawing.Font("Roboto", 11F);
+            this.MessageLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.MessageLabel.Location = new System.Drawing.Point(3, 81);
+            this.MessageLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(0, 19);
+            this.MessageLabel.TabIndex = 4;
+            // 
+            // MessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 160);
+            this.Controls.Add(this.MessageLabel);
+            this.Controls.Add(this.CloseButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MessageForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton CloseButton;
+        private MaterialSkin.Controls.MaterialLabel MessageLabel;
+    }
+}
---
+++ WinForms-ToDoList/Forms/MessageForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace WinForms_ToDoList.Forms
+{
+    public partial class MessageForm : MaterialForm
+    {
+        private readonly MaterialSkinManager skinManager = null;
+
+        public MessageForm()
+        {
+            InitializeComponent();
+
+            MainForm.ThemeSelect(skinManager, this);
+        }
+
+        #region Закрытие окна сообщения
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion
+    }
+}
