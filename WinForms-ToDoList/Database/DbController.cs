afc2aa3a5d3f42d3a7303e7c31511a874c1d0778
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-07-22 08:40:10 +0700

    Adding a method to load data from a database

---
+++ WinForms-ToDoList/Database/DbController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WinForms_ToDoList.Database
+{
+    public static class DbController
+    {
+        #region Переменные для работы с базой данных
+
+        static SQLiteConnection connection = new SQLiteConnection(DbConnection.dbConnection);
+        static SQLiteCommand command;
+        static DataTable tableToDo;
+
+        #endregion
+
+        #region Загрузка данных из базы
+        public static void LoadData(DataGridView dataGridView)
+        {
+            string loadData = "SELECT * FROM ToDoTable";
+            command = new SQLiteCommand(loadData, connection);
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command);
+            tableToDo = new DataTable();
+            dataAdapter.Fill(tableToDo);
+
+            dataGridView.DataSource = tableToDo;
+        }
+        #endregion
+    }
+}
