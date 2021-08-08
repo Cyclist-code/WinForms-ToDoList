be0c8c0809377fd9786c74f1829a3168b81a8b5f
Cyclist-code <47049219+Cyclist-code@users.noreply.github.com>
2021-08-08 07:55:22 +0700

    Adding a method for selecting an application theme

--- WinForms-ToDoList/Services/FormSettings.cs
+++ WinForms-ToDoList/Services/FormSettings.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,26 @@ namespace WinForms_ToDoList.Services
 {
     public static class FormSettings
     {
-
+        #region Выбор темы приложения
+        public static void ChoosingAppTheme(MaterialCheckBox DarkThemeCheckBox)
+        {
+            if (DarkThemeCheckBox.Checked)
+            {
+                //Включение тёмной темы
+                var materialSkinManager = MaterialSkinManager.Instance;
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+                Properties.Settings.Default.DarkTheme = "1";
+                Properties.Settings.Default.Save();
+            }
+            if (!DarkThemeCheckBox.Checked)
+            {
+                //Включение дефолтной темы
+                var materialSkinManager = MaterialSkinManager.Instance;
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+                Properties.Settings.Default.DarkTheme = "0";
+                Properties.Settings.Default.Save();
+            }
+        }
+        #endregion
     }
 }
